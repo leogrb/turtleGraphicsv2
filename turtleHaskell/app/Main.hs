ab894e80472b7a84f3182979010f4a699691ee59
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-11-19 16:35:35 +0100

    prepared haskell setup

--- turtleHaskell/app/Main.hs
+++ turtleHaskell/app/Main.hs
@@ -1,21 +1,35 @@
 module Main where
 
+-- see: https://wiki.haskell.org/OpenGLTutorial1
+
 import Lib
 import Graphics.UI.GLUT
- 
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+import qualified PartOne as PartOne
+
  
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "Functional Languages WS17, Turtle Graphics Project"
   displayCallback $= display
+  reshapeCallback $= Just reshape
   mainLoop
  
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  matrixMode $= Projection
+  loadIdentity
+  ortho2D 0.0 100.0 0.0 100.0
+  matrixMode $= Modelview 0
+  loadIdentity
+  postRedisplay Nothing
+
 display :: DisplayCallback
 display = do 
   clear [ColorBuffer]
-  renderPrimitive Points $
-     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  -- dummy input so far
+  let points = PartOne.runTurtleProgram undefined undefined
+  renderPrimitive LineStrip $
+     mapM_ (\(x, y) -> vertex $ Vertex2 x y) points
   flush
---
+++ turtleHaskell/app/PartOne.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+module PartOne where
+
+import Text.Parsec
+
+type Value = Float
+data Cmd = Forward Value | Left Value | Right Value 
+
+-- completify the domain model...
+
+type Program = [Cmd]
+
+type Vec2 = (Float, Float)
+
+runTurtleProgram :: (Float,Float) -> Program -> [Vec2]
+runTurtleProgram startPos program = 
+    --error "todo implement"
+    [(0.0, 0.0), (6.123031769e-15, 100.0), (98.0, 100.0), (98.0, 4.0), (4.0, 4.0),
+    (4.0, 96.0), (94.0, 96.0), (94.0, 8.0), (8.0, 8.0), (8.0, 92.0), (90.0, 92.0),
+    (90.0, 12.0), (12.0, 12.0), (12.0, 88.0), (86.0, 88.0), (86.0, 16.0),
+    (16.0, 16.0), (16.0, 84.0), (82.0, 84.0), (82.0, 20.0), (20.0, 20.0),
+    (20.0, 80.0), (78.0, 80.0), (78.0, 24.0), (24.0, 24.0), (24.0, 76.0),
+    (74.0, 76.0), (74.0, 28.0), (28.0, 28.0), (28.0, 72.0), (70.0, 72.0),
+    (70.0, 32.0), (32.0, 32.0), (32.0, 68.0), (66.0, 68.0), (66.0, 36.0),
+    (36.0, 36.0), (36.0, 64.0), (62.0, 64.0), (62.0, 40.0), (40.0, 40.0),
+    (40.0, 60.0), (58.0, 60.0), (58.0, 44.0), (44.0, 44.0), (44.0, 56.0),
+    (54.0, 56.0), (54.0, 48.0), (48.0, 48.0), (48.0, 52.0), (50.0, 52.0),
+    (50.0, 52.0)]
+
+stringParser:: Parsec String st String
+stringParser = many anyChar
+
+parseAb :: Parsec String st (Char,Char)
+parseAb = 
+    do 
+        a <- char 'a'
+        b <- char 'b'
+        return (a,b)
+
+simpleParse p = parse p ""
+
+test :: IO ()
+test = do
+    print $ simpleParse stringParser "asdf"
