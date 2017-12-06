a3ce61dac0cadbf47d4d3dd2665723e0e927fb6e
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-12-06 16:59:37 +0100

    initial

--- turtleFSharp/TurtleGraphics/Program.fs
+++ turtleFSharp/TurtleGraphics/Program.fs
@@ -11,15 +11,18 @@ let contextCreated (args : GlControlEventArgs) =
     Gl.MatrixMode(MatrixMode.Modelview)
     Gl.LoadIdentity()
 
+let sw = System.Diagnostics.Stopwatch()
+
 let render (control : Control) (args : GlControlEventArgs) = 
     Gl.Viewport(0, 0, control.ClientSize.Width, control.ClientSize.Height);
     Gl.Clear(ClearBufferMask.ColorBufferBit);
-    
+   
 
     Gl.Begin(PrimitiveType.LineStrip);
 
-    let exampleProgram,desiredStart = PartOne.Examples.quad
-    let points = PartOne.runTurtleProgram desiredStart exampleProgram
+    let exampleProgram,initialState = PartTwo.Examples.star
+    let foodSupply = sw.Elapsed.TotalSeconds * 300.0
+    let points = PartTwo.runTurtleProgram  { initialState with food = foodSupply } exampleProgram
 
     Gl.Color3(1.0f, 1.0f, 1.0f)
     for (x,y) in points do
@@ -32,6 +35,8 @@ let main argv =
     use form = new Form()
     form.Text <- "Functional Languages WS17, Turtle Graphics Project"
     form.Size <- Drawing.Size(800,600)
+
+    sw.Start()
 
     use glControl = new OpenGL.GlControl()
     glControl.Animation <- true
